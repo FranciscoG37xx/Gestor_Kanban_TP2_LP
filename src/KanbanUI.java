686cc63ec0527611eeb0a45e060c415b2ba5e377
FranciscoG37xx <francisco52garrido@gmail.com>
2026-03-26 15:17:36 +0000

    Initial Commit - base UI

---
+++ src/KanbanUI.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author franc
+ */
+import javax.swing.*;
+
+public class KanbanUI extends JFrame {
+
+    public KanbanUI() {
+        setTitle("Kanban");
+        setSize(800, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
